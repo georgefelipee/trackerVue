eb0f26b679ed9612375c7a651adf2f0f9f140323
georgefelipee <georgefelipe938@gmail.com>
2024-04-09 18:52:53 -0300

    start

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "BarraLateral"
+})
+</script>
+
+<template>
+<header>
+  <h1>
+    <img src="@/assets/logo.png" alt="">
+  </h1>
+</header>
+</template>
+
+<style scoped>
+
+header{
+  background-color: #0d3b66;
+  width: 100%;
+  padding: 1rem;
+  height: 100vh;
+}
+@media only screen and (max-width: 768px){
+  header{
+    height: auto;
+    padding: 2.5rem;
+  }
+}
+</style>
